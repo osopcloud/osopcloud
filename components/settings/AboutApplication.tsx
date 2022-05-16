8a678744f0a1923db700686db0cb7deb6a52749c
Noah S <82749151+noahlst@users.noreply.github.com>
2022-05-16 21:18:38 +1000

    Refine About modal, move commit to dedicated page

--- components/settings/AboutApplication.tsx
+++ components/settings/AboutApplication.tsx
@@ -141,7 +141,7 @@ export default function AboutApplication() {
               </Text>
             </Flex>
             <Flex>
-              <Text>Framework</Text>
+              <Text>Technology</Text>
               <Spacer />
               <Text>
                 <Link href="https://nextjs.org">Next.js</Link> on{" "}
--- pages/commit.tsx
+++ pages/commit.tsx
@@ -1,5 +1,6 @@
 // Start page
 export default function CommitDisplayPage() {
   const commit = process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_SHA;
+  console.log(commit);
   return commit ? <code>commit</code> : <p>Commit is undefined</p>;
 }

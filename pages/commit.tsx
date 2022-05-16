6c06240aee2b103268377eda8b8b7f7163471274
Noah S <82749151+noahlst@users.noreply.github.com>
2022-05-16 21:22:13 +1000

    Patch commit page

--- pages/commit.tsx
+++ pages/commit.tsx
@@ -2,5 +2,5 @@
 export default function CommitDisplayPage() {
   const commit = process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_SHA;
   console.log(commit);
-  return commit ? <code>commit</code> : <p>Commit is undefined</p>;
+  return commit ? <code>{commit}</code> : <p>Commit is undefined</p>;
 }

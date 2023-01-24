85617f27abc5999147319b571c98945ba3295b1f
Noah S <82749151+noahlst@users.noreply.github.com>
2023-01-24 16:04:11 +1100

    Update version string

--- components/Version.tsx
+++ components/Version.tsx
@@ -1,1 +1,8 @@
-export const version = "1.0.0-beta.1";
+// Import the app's package.json
+import appPackage from "../package.json";
+
+// The application version string, pulled from package.json
+const version = appPackage.version;
+
+// Make this the default export
+export default version;
